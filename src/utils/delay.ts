de06360d1612044df48af7b8c941aba3473cc200
杨泳樟 <yangyz@mingyuanyun.com>
2023-09-18 23:44:23 +0800

    防抖函数

---
+++ src/utils/delay.ts
@@ -0,0 +1,31 @@
+import { useCallback, useRef } from 'react';
+
+type IDebounce = {
+	(fn: Function, delay: number): Function;
+}
+
+const debounce: IDebounce = (fn, delay) => {
+	let timer: any = null;
+	return (...args: any[]) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			fn(...args);
+		}, delay);
+	}
+}
+
+type IUseDebounce = {
+	(fn: Function, delay: number, dep: any[]): void;
+}
+
+const useDebounce: IUseDebounce = (fn, delay, dep = []) => {
+  const { current } = useRef<Function>(fn);
+
+	return useCallback(
+		debounce((...args: any[]) => current(...args), delay),
+		dep
+	);
+}
+
+
+export { useDebounce };
