c5a38c61c7c8690eb16ee44c140e7b91c28149fb
vincentYz <372963316@qq.com>
2023-10-11 18:47:14 +0800

    logo

--- src/components/poke-page/index.tsx
+++ src/components/poke-page/index.tsx
@@ -5,6 +5,11 @@ import PmType from '../pm-type';
 import styles from './index.module.css';
 import { useNavigate } from 'react-router';
 import i18n from '@/utils/i18n';
+import './sprite.css';
+
+const formatNum = (n: number) => n.toString().padStart(3,'0');
+
+// m=超进化, mx=megaX, my=megaY, gm=极巨化, f=雌性形态, g=嘉乐儿, h=洗翠
 
 type IPokePageProps = {
   data: Pokemon[];
@@ -21,7 +26,9 @@ const PokePage:FC<IPokePageProps> = (props) => {
       {data.map((item, index) => (
         <List.Item key={index} onClick={() => navigate(`/poke-index/${item.national_num}`)} arrow={false}>
           <div className={styles.wrapper}>
-            <div className={styles.prefix}>logo</div>
+            <div className={styles.prefix}>
+              <span className={`sprite-icon sprite-icon-${formatNum(item.national_num)}`}></span>
+            </div>
             <div className={styles.content}>
               <div className={styles.chName}>{i18n.transfer(item)}</div>
               <div className={styles.enName}>{item.en_name}</div>
@@ -30,7 +37,7 @@ const PokePage:FC<IPokePageProps> = (props) => {
                 <PmType id={item.type2} />
               </div>
             </div>
-            <div className={styles.suffix}>No.{item.national_num.toString().padStart(3,'0')}</div>
+            <div className={styles.suffix}>No.{formatNum(item.national_num)}</div>
           </div>
         </List.Item>
       ))}
