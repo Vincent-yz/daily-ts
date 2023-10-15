6846788636a6a850adab5975297f6e871fda8188
杨泳樟 <yangyz@mingyuanyun.com>
2023-10-15 14:54:44 +0800

    player添加特殊标记

--- src/views/king/components/player.tsx
+++ src/views/king/components/player.tsx
@@ -13,10 +13,20 @@ const Player: FC<PlayerProps> = (props) => {
   const { data, onEdit } = props;
   const { name, type1, type2, ability, item, moves, remark } = data;
 
+  /** 狗 */
+  const sturdy: boolean = ability === '结实' || item === '气势披带';
+  const lock: boolean = item === '讲究头带' || item === '讲究眼镜' || item === '讲究围巾';
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.name}>
         {name}
+        {sturdy ?
+          <Tag className={styles.sturdy} color="brown">狗</Tag>
+        : null}
+        {lock ?
+          <Tag className={styles.lock} color="blue">锁</Tag>
+        : null}
         <Tag className={styles.edit} color="default" onClick={() => onEdit(data)}>补充</Tag>
       </div>
       <div className={styles.type}>
